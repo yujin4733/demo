ef53078bf02c678324766107b3bd991123dc2924
yujin4733 <275658575@qq.com>
2021-06-20 21:14:35 +0800

    fix

---
+++ module_mine/src/main/java/debug/MineActivity.kt
@@ -0,0 +1,12 @@
+package debug
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miqulai.mine.R
+
+class MineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.module_mine_activity_mine)
+    }
+}
